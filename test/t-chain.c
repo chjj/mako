3b590a9387f0f352b05872ecf313aa6ceae08d8b
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-09-19 20:47:03 -0400

    chain test

---
+++ test/t-chain.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <string.h>
+#include <node/chain.h>
+#include <satoshi/network.h>
+#include "tests.h"
+
+#ifndef BTC_PREFIX
+#  define BTC_PREFIX "./tmp"
+#endif
+
+int main(void) {
+  btc_chain_t *chain = btc_chain_create(btc_mainnet);
+
+  ASSERT(btc_chain_open(chain, BTC_PREFIX, 20 << 20));
+
+  btc_chain_close(chain);
+  btc_chain_destroy(chain);
+
+  return 0;
+}
