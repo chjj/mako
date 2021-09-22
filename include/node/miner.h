6145091687c56cb809d5e916d693a795a49c03f0
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-09-22 03:31:21 -0400

    more stubs

---
+++ include/node/miner.h
@@ -0,0 +1,23 @@
+/*!
+ * miner.h - miner for libsatoshi
+ * Copyright (c) 2021, Christopher Jeffrey (MIT License).
+ * https://github.com/chjj/libsatoshi
+ */
+
+#ifndef BTC_MINER_H
+#define BTC_MINER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include "types.h"
+#include "../satoshi/common.h"
+#include "../satoshi/types.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BTC_MINER_H */
---
+++ include/node/rpc.h
@@ -0,0 +1,23 @@
+/*!
+ * rpc.h - rpc for libsatoshi
+ * Copyright (c) 2021, Christopher Jeffrey (MIT License).
+ * https://github.com/chjj/libsatoshi
+ */
+
+#ifndef BTC_RPC_H
+#define BTC_RPC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include "types.h"
+#include "../satoshi/common.h"
+#include "../satoshi/types.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BTC_RPC_H */
