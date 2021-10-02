d5560c30d7ac7ac4073c3aeda16e7bb904a7aa1a
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-10-02 15:29:10 -0400

    use stdint for http_parser

--- src/io/http/http_parser.h
+++ src/io/http/http_parser.h
@@ -30,22 +30,7 @@ extern "C" {
 #define HTTP_PARSER_VERSION_PATCH 4
 
 #include <stddef.h>
-#if defined(_WIN32) && !defined(__MINGW32__) && \
-  (!defined(_MSC_VER) || _MSC_VER<1600) && !defined(__WINE__)
-#include <BaseTsd.h>
-typedef __int8 int8_t;
-typedef unsigned __int8 uint8_t;
-typedef __int16 int16_t;
-typedef unsigned __int16 uint16_t;
-typedef __int32 int32_t;
-typedef unsigned __int32 uint32_t;
-typedef __int64 int64_t;
-typedef unsigned __int64 uint64_t;
-#elif (defined(__sun) || defined(__sun__)) && defined(__SunOS_5_9)
-#include <sys/inttypes.h>
-#else
 #include <stdint.h>
-#endif
 
 /* Compile with -DHTTP_PARSER_STRICT=0 to make less checks, but run
  * faster
