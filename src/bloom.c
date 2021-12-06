a17ada1c617af01be538ebe80e5a4980f089209c
Christopher Jeffrey <chjjeffrey@gmail.com>
2021-12-06 13:32:01 -0500

    bloom: minor style fix.

--- src/bloom.c
+++ src/bloom.c
@@ -126,7 +126,7 @@ btc_bloom_add(btc_bloom_t *bloom, const uint8_t *val, size_t len) {
   for (i = 0; i < bloom->n; i++) {
     size_t bit = btc_bloom_hash(bloom, val, len, i);
 
-    bloom->data[bit >> 3] |= (1 << (7 & bit));
+    bloom->data[bit >> 3] |= (1 << (bit & 7));
   }
 }
 
@@ -140,7 +140,7 @@ btc_bloom_has(const btc_bloom_t *bloom, const uint8_t *val, size_t len) {
   for (i = 0; i < bloom->n; i++) {
     size_t bit = btc_bloom_hash(bloom, val, len, i);
 
-    if ((bloom->data[bit >> 3] & (1 << (7 & bit))) == 0)
+    if ((bloom->data[bit >> 3] & (1 << (bit & 7))) == 0)
       return 0;
   }
 
