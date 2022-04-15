b09c3bba2f86a43b1a25f38d191e1d94e5ce78f3
Christopher Jeffrey <chjjeffrey@gmail.com>
2022-04-15 00:35:20 -0400

    json/json_builder: avoid impl-defined behavior.

--- src/json/json_builder.c
+++ src/json/json_builder.c
@@ -133,6 +133,9 @@ json_value * json_array_new (size_t length)
 
     value->type = json_array;
 
+    if (length == 0)
+       length = 1;
+
     if (! (value->u.array.values = (json_value **) malloc (length * sizeof (json_value *))))
     {
        free (value);
@@ -184,6 +187,9 @@ json_value * json_object_new (size_t length)
     ((json_builder_value *) value)->is_builder_value = 1;
 
     value->type = json_object;
+
+    if (length == 0)
+       length = 1;
 
     if (! (value->u.object.values = (json_object_entry *) calloc
            (length, sizeof (*value->u.object.values))))
